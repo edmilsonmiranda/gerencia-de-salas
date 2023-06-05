98cbb24f2978eab156e6c1ce33fcb4f83a34b7dc
edmilson <edneto.mn@gmail.com>
2023-06-05 10:01:41 -0300

    update

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+const Button = (props) => {
+    return(
+        <Container className={[props.outlined && 'outlined', props.pilled && 'pilled']}
+        onClick={props.click}>
+          {props.title}
+        </Container>
+    );
+}
+
+const Container = styled.button`
+    height: 46px;
+    background-color: blueviolet;
+    border-radius: 5px;
+    color: white;
+    text-transform: uppercase;
+    font-size: 12px;
+    padding: 0 26px;
+    border: 2px solid transparent;
+    font-weight: bold;
+    transition-duration: 200ms;
+    &.outlined{
+        border-color: blueviolet;
+        background-color: transparent;
+        color: blueviolet;
+    }
+    &.pilled{
+        border-radius: 25px;
+    }
+    &:hover{
+        background-color: #641da7;
+    }
+`;
+
+export default Button;
