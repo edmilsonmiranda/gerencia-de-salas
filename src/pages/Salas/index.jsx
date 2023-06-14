c6159d10d4c6f62d3c8e89238138f11329c9c67f
edmilson <edneto.mn@gmail.com>
2023-06-14 08:15:42 -0300

    update edit

--- src/pages/Salas/index.jsx
+++ src/pages/Salas/index.jsx
@@ -8,53 +8,101 @@ import { useForm } from "react-hook-form";
 
 const Salas = () => {
 
-    const [lateral, setLateral] = useState(false);
+    const [lateralCriar, setLateralCriar] = useState(false);
+    const [lateralEditar, setLateralEditar] = useState(false);
     const [salas, setSalas] = useState([
       {
+        sala_id: 1,
         sala_nome: 'Bill Gates',
-        sala_capaciade: 20
+        sala_capacidade: 20
       },
       {
+        sala_id: 2,
         sala_nome: 'Steve Jobs',
-        sala_capaciade: 32
+        sala_capacidade: 32
       },
       {
+        sala_id: 3,
         sala_nome: 'Larry Page',
-        sala_capaciade: 32
+        sala_capacidade: 32
       },
     ]);
     const colunas = [
       {
+        name: 'Id',
+        maxWidth: '50px',
+        selector: row => row.sala_id
+      },
+    {
         name: 'Sala',
         selector: row => row.sala_nome,
     },
     {
       name: 'Capacidade',
       selector: row => row.sala_capacidade,
+      maxWidth: '120px',
+      center: true
   },
   {
     name: 'Ações',
-    selector: () => (
-      <>
-        <Button title={'Editar'}/>
-        <Button title={'Deletar'}/>
-      </>
+    maxWidth: '120px',
+    center: true,
+    selector: (row) => (
+      <Actions>
+        <box-icon name='pencil' 
+        onClick={() => {
+          setValue('sala_id', row.sala_id);
+          setValue('sala_nome', row.sala_nome);
+          setValue('sala_capacidade', row.sala_capacidade);
+          setLateralEditar(true)}} ></box-icon>
+        <box-icon 
+        name='trash'
+        onClick={() => deletarSala(row.sala_id)}></box-icon>
+      </Actions>
     )
   }
 ]
     
     const { register, handleSubmit } = useForm();
+    const { register: registerEditar, handleSubmit: handleSubmitEditar, setValue} = useForm();
 
     const cadastrarSala = (dados) => {
+      dados.sala_id = salas.length + 1;it 
       setSalas([...salas, dados]);
-      setLateral(false);
+      setLateralCriar(false);
+    }
+
+    const atualizarSala = (dados) => {
+      const salasAtualizadas = salas.map((sala) => {
+        if(sala.sala_id === dados.sala_id){
+          let novaSala = {
+            sala_id: dados.sala_id,
+            sala_nome: dados.sala_nome,
+            sala_capacidade: dados.sala_capacidade,
+          }
+          return novaSala;
+        }
+        return sala;
+      });
+      setSalas(salasAtualizadas);
+      setLateralEditar(false);
+    }
+
+    const deletarSala = (sala_id) => {
+      const salasRestantes = salas.filter(sala => {
+        if(sala.sala_id !== sala_id){
+          return sala;
+        }
+      });
+      setSalas(salasRestantes);
+      setLateralEditar(false);
     }
 
     return(
       <>
        <Header>
         <h1>Salas</h1>
-        <Button title="Nova sala" click={() => setLateral(true)}/>
+        <Button title="Nova sala" click={() => setLateralCriar(true)}/>
        </Header>
        
 
@@ -63,14 +111,19 @@ const Salas = () => {
         columns={colunas} />
 
       
-      <Overlay className={lateral && 'active'} onClick={() => setLateral(false)}/>
-        <Lateral className={lateral && 'active'}>
+      <Overlay className={lateralCriar || lateralEditar ? 'active' : ''} 
+      onClick={() => {
+        setLateralCriar(false);
+        setLateralEditar(false);
+        }}/>
+
+        <Lateral className={lateralCriar && 'active'}>
           <Header>
             <h3>Adicionar</h3>
             <Button 
             title={'X'} 
             classes={'circle pilled'} 
-            click={() => setLateral(false)}/>
+            click={() => setLateralCriar(false)}/>
           </Header>
           <Body>
             <form onSubmit={handleSubmit(cadastrarSala)}>
@@ -80,6 +133,28 @@ const Salas = () => {
              <Label>Capacidade</Label>
              <Input type="number" 
              {...register ('sala_capacidade')} />
+             <Button title={'Salvar'} classes={'telatoda'} type='submit'/>
+            </form>
+          </Body>
+        </Lateral>
+
+        <Lateral className={lateralEditar && 'active'}>
+          <Header>
+            <h3>Adicionar</h3>
+            <Button 
+            title={'X'} 
+            classes={'circle pilled'} 
+            click={() => setLateralEditar(false)}/>
+          </Header>
+          <Body>
+            <form onSubmit={handleSubmitEditar(atualizarSala)}>
+             <Input type="hidden" { ...registerEditar('sala_editar')}/>
+             <Label>Nome</Label>
+             <Input placeholder="Digite o nome da sala" 
+             {...registerEditar ('sala_nome')} />
+             <Label>Capacidade</Label>
+             <Input type="number" 
+             {...registerEditar ('sala_capacidade')} />
              <Button title={'Salvar'} classes={'telatoda'} type='submit'/>
             </form>
           </Body>
@@ -160,5 +235,28 @@ const Salas = () => {
           border-color: blueviolet;
         }
     `;
+
+    const Actions = styled.div`
+        display: flex;
+        justify-content: center;
+        gap: 16px;
+        align-items: center;
+        & box-icon{
+          width: 40px;
+          height: 40px;
+          line-height: 40px;
+          text-align: center;
+          padding: 8px;
+          background-color: blueviolet;
+          fill: white;
+          cursor: pointer;
+          border-radius: 20px;
+          transition-duration: 200ms;
+        }
+        & box-icon:hover{
+          background-color: #641da7;
+        }
+    
+    `
   
 export default Salas;
